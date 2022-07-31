317ef79ac09b432f9cb9a37ba6cd6690b9172191
AdrianFagilde <adrianfagilde@gmail.com>
2022-07-31 19:28:16 -0400

    I added the leaf function

--- main.cpp
+++ main.cpp
@@ -11,10 +11,14 @@ int main() {
 	Files file;
 	vector<string> aux = file.read();
     BinaryTree<const char*> bTree(aux[0].c_str());
-    bTree.insert("zorro");
-    bTree.insert("manzana");
-    bTree.insert("hilo");
-    bTree.insert("casa");
-    bTree.postOrder();
+    
+    for(int i = 1 ; i < aux.size() ; i++ )
+	{
+		bTree.insert(aux[i].c_str());
+	}
+	bTree.postOrder();
+	
+	cout << "" << endl;
+    bTree.sheets();
     return 0;
 }
